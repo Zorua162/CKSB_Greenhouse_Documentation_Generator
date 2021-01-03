5e8448de76ca609462076fdbb7d76dbdee3410aa
DarkKronicle <darkkronicle@gmail.com>
2021-01-02 18:39:34 -0700

    Adding images saturator

--- gen_greenhouse_doc_from_config.py
+++ gen_greenhouse_doc_from_config.py
@@ -1,8 +1,4 @@
 # Module for editing docx files
-from colorsys import rgb_to_hls, hls_to_rgb
-import cv2
-import numpy as np
-from skimage import io
 from tqdm import tqdm
 
 
@@ -22,9 +18,9 @@ import warnings
 
 
 def humanify(string):
-    '''
+    """
     Lower, capitalize and set underscores to spaces in given string
-    '''
+    """
     return string.lower().capitalize().replace('_', ' ')
 
 
@@ -82,95 +78,10 @@ def add_fluid_requirement(biome, paragraph):
                 paragraph.add_run(f'\n  No {fluid.capitalize()}')
 
 
-def fix_channels(img, fix_invert=True, force_trans=-1):
-    """
-    Forces images to be in RGBA format.
-    """
-    try:
-        height, width, channels = img.shape
-    except ValueError:
-        height, width = img.shape
-        channels = 1
-    pixels = np.int8(img.reshape(width, -1, channels))
-    oned = []
-    if channels == 1:
-        for w in range(width):
-            for h in range(height):
-                pixel = pixels[w, h] % 255
-                if force_trans < 0:
-                    force_trans = 255
-                oned.extend([pixel, pixel, pixel, force_trans])
-    else:
-        for w in range(width):
-            # oned = []
-            for h in range(height):
-                if channels == 4 and force_trans < 0:
-                    pixel = pixels[w, h]
-                    if fix_invert:
-                        oned.extend([pixel[2] % 255, pixel[1] % 255, pixel[0] % 255, pixel[3] % 256])
-                    else:
-                        oned.extend([pixel[0] % 255, pixel[1] % 255, pixel[2] % 255, pixel[3] % 256])
-                else:
-                    pixel = pixels[w, h]
-                    if force_trans < 0:
-                        force_trans = 255
-                    if fix_invert:
-                        oned.extend([pixel[2] % 255, pixel[1] % 255, pixel[0] % 255, force_trans])
-                    else:
-                        oned.extend([pixel[0] % 255, pixel[1] % 255, pixel[2] % 255, force_trans])
-        # last.append(oned)
-    ar = np.array(oned)
-    ar = ar.reshape(height, -1, 4)
-    return ar
-
-
-def map_sat(r1, g1, b1, r2, g2, b2):
-    h1, l1, s1 = rgb_to_hls(r1 / 255, g1 / 255, b1 / 255)
-    h2, l2, s2 = rgb_to_hls(r2 / 255, g2 / 255, b2 / 255)
-    r3, g3, b3 = hls_to_rgb(h2, l1, (s1 + s2) / 2)
-    return int(r3 * 255), int(g3 * 255), int(b3 * 255)
-
-
-def color_image(img, r, g, b):
-    pixels = fix_channels(io.imread(img))
-    height, width, channels = img.shape
-    img_rows = []
-    img_stitched = None
-    for h in range(height):
-        row_image = None
-        for w in range(width):
-            p = pixels[h, w]
-            a = p[3] % 256
-            if a > 0:
-                # If we don't copy, it actually gets modified.
-                i = p.copy()
-                new = map_sat(i[0], i[1], i[2], r, g, b)
-                i[0] = new[0]
-                i[1] = new[1]
-                i[2] = new[2]
-                i[3] = a
-            else:
-                i = p
-            i = i.reshape((1, 1, 4))
-            if row_image is None:
-                row_image = i
-            else:
-                row_image = np.concatenate((row_image, i), axis=1)
-
-        img_rows.append(row_image)
-    for i in img_rows:
-        if img_stitched is None:
-            img_stitched = i
-        else:
-            img_stitched = np.concatenate((img_stitched, i), axis=0)
-
-    return img
-
-
 def create_doc():
-    '''
+    """
     Creates the Greenhouse Biomes Documentation
-    '''
+    """
     # Creates a blank template document
     document = Document()
     # for style in document.styles:
@@ -186,7 +97,8 @@ def create_doc():
                      'won’t spawn in the greenhouse.',
         'Floor Coverage': 'How much of the floor needs to be that kind of block. ',
         'Conversions': 'What blocks will convert to another block.',
-        'Biome': 'What biome it will be inside of the greenhouse.'}
+        'Biome': 'What biome it will be inside of the greenhouse.'
+    }
 
     # Assume that the config to use is ./biomes.yml
     # file_to_use = input('Enter the name of the biomes.yml to use >')
@@ -199,7 +111,7 @@ def create_doc():
 
     # Not working warning:
     strikethrough = document.add_paragraph('')
-    run = strikethrough.add_run('# strike through highlighted text ' +
+    run = strikethrough.add_run('# strike through highlighted text '
                                 '= not functioning')
     run.italics = True
     run.font.highlight_color = WD_COLOR_INDEX.YELLOW
@@ -209,7 +121,7 @@ def create_doc():
     # Write this date to the file as the last updated date
     updated_paragraph = document.add_paragraph('')
     updated_paragraph.alignment = WD_ALIGN_PARAGRAPH.RIGHT
-    run = updated_paragraph.add_run('Public Doc\nLast ' +
+    run = updated_paragraph.add_run('Public Doc\nLast '
                                     f'updated: {today}')
     run.italic = True
     run.font.size = Pt(8)
---
+++ images.py
@@ -0,0 +1,115 @@
+from colorsys import rgb_to_hls, hls_to_rgb
+import numpy as np
+from skimage import io
+
+
+def fix_channels(img, fix_invert=True, force_trans=-1):
+    """
+    Forces images to be in RGBA format.
+    """
+    try:
+        height, width, channels = img.shape
+    except ValueError:
+        height, width = img.shape
+        channels = 1
+    pixels = np.int8(img.reshape(width, -1, channels))
+    oned = []
+    if channels == 1:
+        for w in range(width):
+            for h in range(height):
+                pixel = pixels[w, h] % 255
+                if force_trans < 0:
+                    force_trans = 255
+                oned.extend([pixel, pixel, pixel, force_trans])
+    else:
+        for w in range(width):
+            # oned = []
+            for h in range(height):
+                if channels == 4 and force_trans < 0:
+                    pixel = pixels[w, h]
+                    if fix_invert:
+                        oned.extend([pixel[2] % 255, pixel[1] % 255, pixel[0] % 255, pixel[3] % 256])
+                    else:
+                        oned.extend([pixel[0] % 255, pixel[1] % 255, pixel[2] % 255, pixel[3] % 256])
+                else:
+                    pixel = pixels[w, h]
+                    if force_trans < 0:
+                        force_trans = 255
+                    if fix_invert:
+                        oned.extend([pixel[2] % 255, pixel[1] % 255, pixel[0] % 255, force_trans])
+                    else:
+                        oned.extend([pixel[0] % 255, pixel[1] % 255, pixel[2] % 255, force_trans])
+        # last.append(oned)
+    ar = np.array(oned)
+    ar = ar.reshape(height, -1, 4)
+    return ar
+
+
+def map_sat(r1, g1, b1, r2, g2, b2):
+    h1, l1, s1 = rgb_to_hls(r1 / 255, g1 / 255, b1 / 255)
+    h2, l2, s2 = rgb_to_hls(r2 / 255, g2 / 255, b2 / 255)
+    r3, g3, b3 = hls_to_rgb(h2, l1, (s1 + s2) / 2)
+    return int(r3 * 255), int(g3 * 255), int(b3 * 255)
+
+
+def color_image(img, r, g, b):
+    pixels = fix_channels(img)
+    height, width, channels = img.shape
+    img_rows = []
+    img_stitched = None
+    for h in range(height):
+        row_image = None
+        for w in range(width):
+            p = pixels[h, w]
+            a = p[3] % 256
+            if a > 0:
+                # If we don't copy, it actually gets modified.
+                i = p.copy()
+                new = map_sat(i[0], i[1], i[2], r, g, b)
+                i[0] = new[0]
+                i[1] = new[1]
+                i[2] = new[2]
+                i[3] = a
+            else:
+                i = p
+            i = i.reshape((1, 1, 4))
+            if row_image is None:
+                row_image = i
+            else:
+                row_image = np.concatenate((row_image, i), axis=1)
+
+        img_rows.append(row_image)
+    for i in img_rows:
+        if img_stitched is None:
+            img_stitched = i
+        else:
+            img_stitched = np.concatenate((img_stitched, i), axis=0)
+
+    return img
+
+
+def color_image_checked(img, r, g, b):
+    if not is_grayscale(img):
+        return img
+    return color_image(img, r, g, b)
+
+
+def load_and_color(img_path, r, g, b):
+    img = io.imread(img_path)
+    return color_image_checked(img, r, g, b)
+
+
+def is_grayscale(img):
+    pixels = img
+    height, width, channels = img.shape
+    if channels == 1:
+        return True
+    for h in range(height):
+        for w in range(width):
+            p = pixels[h, w]
+            if p[0] == p[1] == p[2]:
+                continue
+            else:
+                return False
+    return True
+
